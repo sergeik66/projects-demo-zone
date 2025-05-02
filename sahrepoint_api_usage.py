7ad7b5e65fe1540eadb1d9e8f0f7978b398d7c32
skitp <skitp66@gmail.com>
2025-05-02 11:19:22 -0400

    Update sahrepoint_api_usage.py

--- sahrepoint_api_usage.py
+++ sahrepoint_api_usage.py
@@ -1,3 +1,23 @@
+from api_client.core.sharepoint_api import SharePointAPI
+from api_client.core.utils import save_sharepoint_file_to_lakehouse
+
+sp_api = SharePointAPI(
+    tenant_id="YOUR_TENANT_ID",
+    client_id="YOUR_CLIENT_ID",
+    client_secret="YOUR_CLIENT_SECRET",
+    site_url="https://yourtenant.sharepoint.com/sites/yoursite",
+    user_agent=USER_AGENT
+)
+
+content = sp_api.get_file_by_server_relative_url(
+    "/sites/yoursite/Shared Documents/myfile.docx",
+    get_content=True
+)
+if content:
+    save_sharepoint_file_to_lakehouse(content, "/lakehouse/default/Files/myfile.docx")
+
+
+
 # Get file metadata
 metadata = sp_api.get_file_by_server_relative_url(
     "/sites/yoursite/Shared Documents/myfile.docx",
