e28de26a029ce21e43a437a2651fb3883bcf3154
skitp <skitp66@gmail.com>
2025-05-30 13:01:35 -0400

    Update test_private_key_passphrase.py

--- test_private_key_passphrase.py
+++ test_private_key_passphrase.py
@@ -18,3 +18,18 @@ pgp = PGP(
 input_file = "abfss://.../Files/user_data/pdos_webreport/decrypted/BOP PD & OS.xlsx"
 output_path = "abfss://.../Files/user_data/pdos_webreport/encrypted"
 pgp.encrypt_file(input_file, output_path)  # Re-encrypt to ensure key match
+
+
+from spark_engine.common.pgp import PGP
+pgp = PGP(
+    key_vault_name="my-key-vault",
+    public_key_secret="mock-public-key-secret",
+    private_key_secret="mock-private-key-secret",
+    passphrase_secret="mock-passphrase-secret"
+)
+test_file = "abfss://.../Files/templates/emails/decrypted/test.txt"
+with fs.open(test_file, "wb") as f:
+    f.write(b"Test content")
+pgp.encrypt_file(test_file, encrypted_folder)
+encrypted_file = f"{encrypted_folder}/test.txt.pgp"
+pgp.decrypt_file(encrypted_file, decrypted_folder)
